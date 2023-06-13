6d07152612119f0ee04317e57a277572f74752e7
Jeong Kiyeon <ions@kakao.com>
2023-06-13 11:35:11 +0900

    기본 구조 추가 및 .gitignore 변경
    CL(Class Library, .dll) 추가
    bin, obj, .suo 파일 무시 추가
    산출물 Files 폴더로 변경

---
+++ WindowsShortCutsPackage/WIN_SHORTCUTS_CL/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WIN_SHORTCUTS_CL.Forms;
+
+namespace WIN_SHORTCUTS_CL
+{
+    public class main
+    {
+        Forms.WSC_MAIN
+    }
+}
